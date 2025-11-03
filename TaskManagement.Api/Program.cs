0083a0e1da4c510afc13ce31aad84c2ae9305563
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2025-11-03 16:51:44 +0200

    Setup the project

---
+++ TaskManagement.Api/Program.cs
@@ -0,0 +1,20 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
