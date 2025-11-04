ee85fe7bdb3d23e1f4fae04d49224900d4eacf1d
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2025-11-04 06:43:04 +0200

    Configure DbContext with SQLite in Program.cs

--- TaskManagement.Api/Program.cs
+++ TaskManagement.Api/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
 using Scalar.AspNetCore;
+
+using TaskManagement.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlite("Data Source=data.db");
+});
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
