0b06bdce61ecee7588f77fe9bb467ee136a22210
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2025-11-04 11:14:54 +0200

    Add TaskController and PatchTaskItemDto for task management functionality

--- TaskManagement.Core/Dtos/PatchTaskItemDto.cs
+++ TaskManagement.Core/Dtos/PatchTaskItemDto.cs
@@ -4,14 +4,12 @@ using TaskManagement.Core.Models;
 
 namespace TaskManagement.Core.Dtos;
 
-public class TaskItemDto
+public class PatchTaskItemDto
 {
-    [Required, MinLength(1), MaxLength(150)]
     public string? Title { get; set; }
 
-    [MinLength(1), MaxLength(500)]
     public string? Description { get; set; }
 
     [Range(0, 2, ErrorMessage = "Invalid status value.")]
-    public TaskItemStatus Status { get; set; }
+    public TaskItemStatus? Status { get; set; }
 }
