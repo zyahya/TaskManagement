7b104fee97897774b8c9a735137c49829cf28ecb
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2025-11-04 11:15:05 +0200

    Implement TaskRepository for task management operations

---
+++ TaskManagement.Data/Repositories/TaskRepository.cs
@@ -0,0 +1,77 @@
+using Microsoft.EntityFrameworkCore;
+
+using TaskManagement.Core.Dtos;
+using TaskManagement.Core.Interfaces;
+using TaskManagement.Core.Models;
+
+namespace TaskManagement.Data.Repositories;
+
+public class TaskRepository : ITaskRepository
+{
+    private AppDbContext _context;
+
+    public TaskRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<TaskItem> CreateAsync(TaskItemDto taskItemDto)
+    {
+        var taskItem = new TaskItem
+        {
+            Title = taskItemDto.Title,
+            Description = taskItemDto.Description,
+            Status = taskItemDto.Status
+        };
+        await _context.Tasks.AddAsync(taskItem);
+        await _context.SaveChangesAsync();
+
+        return taskItem;
+    }
+
+    public async Task<TaskItem?> Delete(int id)
+    {
+        var item = await _context.Tasks.FindAsync(id);
+        if (item == null) return null;
+
+        _context.Remove(item);
+        await _context.SaveChangesAsync();
+
+        return item;
+    }
+
+    public async Task<ICollection<TaskItem>> GetAllAsync()
+    {
+        var items = await _context.Tasks.ToListAsync();
+        return items;
+    }
+
+    public async Task<TaskItem?> GetByIdAsync(int id)
+    {
+        var item = await _context.Tasks.FindAsync(id);
+        if (item == null) return null;
+
+        return item;
+    }
+
+    public async Task PatchUpdateAsync(TaskItem item)
+    {
+        _context.Tasks.Update(item);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<TaskItem?> UpdateAsync(int id, TaskItemDto taskItemDto)
+    {
+        var item = await _context.Tasks.FindAsync(id);
+        if (item == null) return null;
+
+        item.Title = taskItemDto.Title;
+        item.Description = taskItemDto.Description;
+        item.Status = taskItemDto.Status;
+
+        _context.Tasks.Update(item);
+        await _context.SaveChangesAsync();
+
+        return item;
+    }
+}
