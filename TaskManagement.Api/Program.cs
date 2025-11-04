0bf0fced84eeacbf3ca399af101072361ecc37d6
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2025-11-04 07:07:17 +0200

    Register ITaskRepository

--- TaskManagement.Api/Program.cs
+++ TaskManagement.Api/Program.cs
@@ -2,7 +2,9 @@ using Microsoft.EntityFrameworkCore;
 
 using Scalar.AspNetCore;
 
+using TaskManagement.Core.Interfaces;
 using TaskManagement.Data;
+using TaskManagement.Data.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 });
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
 
 var app = builder.Build();
 
