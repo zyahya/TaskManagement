d503d92c38769e315eb82a431660c220feb5c59c
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2025-11-06 17:41:36 +0200

    Refactor User model to enforce validation attributes on properties

--- TaskManagement.Core/Models/User.cs
+++ TaskManagement.Core/Models/User.cs
@@ -1,11 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TaskManagement.Core.Models;
 
 public class User
 {
+    [Key]
     public int Id { get; set; }
+
+    [Required, MinLength(3), MaxLength(14)]
     public string? Username { get; set; }
+
+    [Required, MinLength(3), MaxLength(20)]
     public string? FirstName { get; set; }
+
+    [Required, MinLength(1), MaxLength(20)]
     public string? LastName { get; set; }
+
+    [Required, EmailAddress]
     public string? Email { get; set; }
-    public string? PasswordHash { get; set; }
+
+    [Required, MinLength(1), MaxLength(256)]
+    public string PasswordHash { get; set; }
 }
