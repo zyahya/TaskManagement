d2463f90c566f1cf31f1b2c497ddaa734acd277b
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2025-11-10 10:57:50 +0200

    Remove commented-out properties from User model for cleaner code

--- TaskManagement.Core/Models/User.cs
+++ TaskManagement.Core/Models/User.cs
@@ -10,15 +10,6 @@ public class User
     [Required, MinLength(3), MaxLength(14)]
     public string Username { get; set; }
 
-    // [Required, MinLength(3), MaxLength(20)]
-    // public string? FirstName { get; set; }
-
-    // [Required, MinLength(1), MaxLength(20)]
-    // public string? LastName { get; set; }
-
-    // [Required, EmailAddress]
-    // public string? Email { get; set; }
-
     [Required, MinLength(1), MaxLength(256)]
     public string PasswordHash { get; set; }
 
