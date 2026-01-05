9311cd97057b3e978ee28dfd3a5add3d3b93ac52
Ziad Yahya <122354204+zyahya@users.noreply.github.com>
2026-01-05 09:11:41 +0200

    Add OnModelCreating method to configure default value for TaskItem.Status property

--- TaskManagement.Data/AppDbContext.cs
+++ TaskManagement.Data/AppDbContext.cs
@@ -8,6 +8,13 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaskItem>()
+            .Property(ti => ti.Status)
+            .HasDefaultValue(0);
+    }
+
     public DbSet<TaskItem> Tasks { get; set; }
     public DbSet<User> Users { get; set; }
 }
